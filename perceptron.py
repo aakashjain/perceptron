65719db79c95407466b5427fc694956714bf144e
Aakash Jain <aakashj19@gmail.com>
2015-12-11 12:04:58 +0530

    make bias independent of feature vec, allow passing array of samples to classify

--- perceptron.py
+++ perceptron.py
@@ -2,26 +2,41 @@ import numpy as np
 
 
 class Perceptron:
-    def __init__(self, learning_rate=0.5, threshold=0):
+    def __init__(self, learning_rate=0.5, threshold=0, bias=0):
         self.learning_rate = learning_rate
         self.threshold = threshold
+        self.bias = bias
         self.weights = []
 
-    def classify(self, x, labels = [0, 1]):
-        return labels[1] if self.feed_forward(x) > 0 else labels[0]
+    def classify(self, X, labels = [0, 1]):
+        y = []
+        for Xi in X:
+            result = self.feed_forward(Xi)
+            if result == 1:
+                y.append(labels[1])
+            else:
+                y.append(labels[0])
+        return y
 
-    def feed_forward(self, x):
-        return 1 if np.dot(x + [1], self.weights) > self.threshold else -1
+    def feed_forward(self, X):
+        value = self.bias
+        for i in range(len(X)):
+            value += X[i] * self.weights[i]
+        if value > self.threshold:
+            return 1
+        else:
+            return -1
 
-    def train(self, Xs, ys, epochs=10):
-        self.weights = np.random.uniform(-1, 1, len(Xs[0])+1)
+    def train(self, X, y, epochs=10):
+        vec_len = len(X[0])
+        self.weights = np.random.uniform(-1, 1, vec_len)
         for _ in range(epochs):
-            for i in range(len(Xs)):
-                _y = self.feed_forward(Xs[i])
-                _x = Xs[i] + [1]
-                err = ys[i] - _y
-                self.weights = [self.learning_rate * err * _x[j]
-                                + self.weights[j] for j in range(len(_x))]
+            for i in range(len(X)):
+                yp = self.feed_forward(X[i])
+                err = y[i] - yp
+                self.bias += self.learning_rate * err
+                for j in range(vec_len):
+                    self.weights[j] += self.learning_rate * err * X[i][j]
 
 
 if __name__ == '__main__':
@@ -32,5 +47,4 @@ if __name__ == '__main__':
          [1,1]]
     y = [-1, -1, -1, 1]
     p.train(X, y)
-    for x in X:
-        print p.classify(x)
+    print p.classify(X)
